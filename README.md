d08b361f576b59cadf1c536775a0abc061994f5c
CodeNMore <jackofall55@gmail.com>
2017-02-06 14:08:33 -0500

    Initial Commit of Old Site

---
+++ README.md
@@ -0,0 +1,2 @@
+# CodeNMore Site
+The CodeNMore website was moved to Github on Febuary 6th, 2017, and is the official website for the CodeNMore YouTube channel.
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("header").load("header.html");
+  $(".sidebar").load("sidebar.html");
+  $("footer").load("footer.html");
+});
